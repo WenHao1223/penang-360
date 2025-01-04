083ecdd8dd4abddf0ea41591da0fbe86ef3631a0
WenHao1223 <wenhaojshs@gmail.com>
2025-01-04 16:12:47 +0800

    Setup heroicons

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import "./App.css";
 import 'boxicons'
+import { BeakerIcon } from '@heroicons/react/24/solid'
 
 function App() {
     return (
         <>
             <h1 className="text-3xl font-bold underline">Hello world! <box-icon name='like' color="white" size='lg'></box-icon></h1>
             <i className="fas fa-kitchen-set"></i>
+            <BeakerIcon className="size-6 text-blue-500" />
             <div className="carousel carousel-vertical rounded-box h-96">
                 <div className="carousel-item h-full">
                     <img src="https://img.daisyui.com/images/stock/photo-1559703248-dcaaec9fab78.webp" />
