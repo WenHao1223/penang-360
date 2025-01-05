4368aa9a4899318aa66835a379119050c1670d20
WenHao1223 <wenhaojshs@gmail.com>
2025-01-06 02:30:51 +0800

    Change text

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -115,7 +115,7 @@ const Hero: React.FC = () => {
                                 href="#places-of-interest"
                                 className="text-sm/6 font-semibold text-gray-900"
                             >
-                                Places of Interest
+                                Attractions
                             </a>
                             <a
                                 href="#dining"
@@ -184,7 +184,7 @@ const Hero: React.FC = () => {
                                             href="#places-of-interest"
                                             className="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-gray-900 hover:bg-gray-50"
                                         >
-                                            Places of Interest
+                                            Attractions
                                         </a>
                                         <a
                                             href="#dining"
