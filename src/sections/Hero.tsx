f7e279ef54b6fbd8066f7b3f5328cd311f6e0a0a
WenHao1223 <wenhaojshs@gmail.com>
2025-01-06 09:20:26 +0800

    Change compan name

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -84,7 +84,7 @@ const Hero: React.FC = () => {
                     >
                         <div className="flex lg:flex-1">
                             <a href="#" className="-m-1.5 p-1.5">
-                                <span className="sr-only">Your Company</span>
+                                <span className="sr-only">Penang 360°</span>
                                 <img
                                     className="h-8 w-auto"
                                     src={LogoTrimmed}
@@ -151,9 +151,7 @@ const Hero: React.FC = () => {
                         <div className="sideBar fixed inset-y-0 right-0 z-30 w-full h-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
                             <div className="flex items-center justify-between">
                                 <a href="#" className="-m-1.5 p-1.5">
-                                    <span className="sr-only">
-                                        Your Company
-                                    </span>
+                                    <span className="sr-only">Penang 360°</span>
                                     <img
                                         className="h-8 w-auto object-contain"
                                         src={LogoTrimmed}
