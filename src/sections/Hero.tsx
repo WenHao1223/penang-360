c5ef1acd98b20dfb1f9d9c5d31442d0bf1c14303
WenHao1223 <wenhaojshs@gmail.com>
2025-01-06 18:15:21 +0800

    Add min height

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -228,8 +228,8 @@ const Hero: React.FC = () => {
                                     "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
                             }}
                         />
-                    </div>
-                    <div className="absolute inset-x-0 top-0 -z-20 h-screen overflow-hidden mix-blend-overlay">
+                    </div>75
+                    <div className="absolute inset-x-0 top-0 -z-20 h-screen overflow-hidden mix-blend-overlay min-h-[720px]">
                         <div
                             ref={imageRef}
                             className="relative w-full h-full opacity-30 blur-sm"
