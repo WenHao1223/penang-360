54eac7e3306d68b2d7d225af80dddf4f31d99dbf
WenHao1223 <wenhaojshs@gmail.com>
2025-01-07 04:47:57 +0800

    Fix intro issue

--- src/sections/Intro.tsx
+++ src/sections/Intro.tsx
@@ -6,12 +6,12 @@ import PenangStreet from "@assets/images/intro/penang-street.webp";
 const Intro: React.FC = () => {
     return (
         <section
-            className="section bg-white pt-14 px-16 md:px-48 lg:px-64 text-gray-800 h-screen"
+            className="section bg-white pt-10 px-16 md:px-32 lg:px-64 text-gray-800 min-h-screen h-screen"
             id="intro"
         >
             <div className="flex flex-col md:flex-row gap-12">
                 <div className="w-full md:w-1/2">
-                    <div className="relative h-96 xl:h-[60vh] overflow-hidden mb-6 mix-blend-multiply">
+                    <div className="relative h-96 md:h-80 xl:h-[60vh] overflow-hidden mb-6 mix-blend-multiply">
                         <div className="absolute inset-0 w-full h-full bg-white opacity-10 z-10 mix-blend-luminosity"></div>
                         <img
                             src={PenangStreet}
@@ -42,7 +42,7 @@ const Intro: React.FC = () => {
                         to an array of heritage buildings, historical landmarks,
                         and a lively blend of diverse cultures.
                     </p>
-                    <div className="mt-6 relative h-96 xl:h-[60vh] overflow-hidden">
+                    <div className="mt-6 relative h-96 md:h-80 xl:h-[60vh] overflow-hidden">
                         <div className="absolute inset-0 w-full h-full bg-white opacity-10 z-10 mix-blend-luminosity"></div>
                         <img
                             src={Komtar}
