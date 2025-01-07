a41537e91889399ba44ed46f00729ccad3463c6c
WenHao1223 <wenhaojshs@gmail.com>
2025-01-08 02:01:22 +0800

    Change color

--- src/sections/Food.tsx
+++ src/sections/Food.tsx
@@ -17,7 +17,7 @@ const Food: React.FC = () => {
 
     return (
         <div className="section relative pt-10 px-16 md:px-32 lg:px-64 min-h-screen flex flex-col items-center justify-center gap-0">
-            <div className="absolute inset-0 bg-gradient-to-tr from-pink-50 to-white z-0"></div>
+            <div className="absolute inset-0 bg-gradient-to-tr from-pink-100 to-white z-0"></div>
             <div className="z-10 flex flex-col items-center justify-center gap-4">
                 <h1 ref={titleRef} className="text-4xl font-bold text-center opacity-0 centa-one text-black">
                     Food & Dining
