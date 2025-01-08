3f7458fd321de16dcd4897ae2c4220cd97d74266
WenHao1223 <wenhaojshs@gmail.com>
2025-01-08 20:01:00 +0800

    Setup contact sections

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import Video from "@sections/Video";
 import Attractions from "@sections/Attractions";
 import Food from "@sections/Food";
 import Hotel from "@sections/Hotel";
+import Contact from "@sections/Contact";
 
 function App() {
     return (
@@ -16,6 +17,7 @@ function App() {
             <Attractions/>
             <Food/>
             <Hotel/>
+            <Contact/>
         </>
     );
 }
