34a1d23e00ab8ada0ce4188bd54a7d6da69c5431
WenHao1223 <wenhaojshs@gmail.com>
2025-01-09 19:40:07 +0800

    Resolve path

--- vite.config.ts
+++ vite.config.ts
@@ -15,6 +15,6 @@ export default defineConfig({
     },
   },
   build: {
-    outDir: 'dist',  // This will specify the output folder as 'dist'
+    outDir: path.resolve(__dirname, 'dist'),  // This will specify the output folder as 'dist'
   },
 })
